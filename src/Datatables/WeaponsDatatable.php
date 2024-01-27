e0b2405f882cf0840244c8b9252ac205e82939d0
Atlis <atlis131@gmail.com>
2024-01-27 15:41:01 +0100

    Upgrade Symfony from 5.4 to 6.4, Minor code refactor

--- src/Datatables/WeaponsDatatable.php
+++ src/Datatables/WeaponsDatatable.php
@@ -6,22 +6,13 @@ use App\Entity\Weapon;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface as Container;
 
-class WeaponsDatatable
+class WeaponsDatatable extends Datatable
 {
-    private ?int                   $firstRecord;
-    private ?string                $search      = null;
-    private ?int                   $recordsCount;
-    private ?array                 $orderColumn = null;
-    private ?array                 $filters     = [];
-    private EntityManagerInterface $em;
-    private Container              $container;
+    private Container $container;
 
-    public function __construct(
-        EntityManagerInterface $em,
-        Container              $container
-    )
+    public function __construct(EntityManagerInterface $em, Container $container)
     {
-        $this->em = $em;
+        parent::__construct($em);
         $this->container = $container;
     }
 
@@ -75,34 +66,5 @@ class WeaponsDatatable
             'recordsFiltered' => $records['filtered'],
             'data'            => $records['data'],
         ];
-    }
-
-    private function processRequest($request): void
-    {
-        $this->firstRecord = $request->get('start');
-        $this->recordsCount = $request->get('length');
-
-        $columns = $request->get('columns');
-
-        if (!is_null($request->get('order')) && isset($request->get('order')[0])) {
-            $this->orderColumn = [
-                'column' => $columns[$request->get('order')[0]['column']]['data'],
-                'dir'    => $request->get('order')[0]['dir']
-            ];
-        }
-
-        if (!is_null($request->get('search')) && $request->get('search')['value'] !== '') {
-            $this->search = $request->get('search')['value'];
-        }
-
-        if ($request->get('weaponType') != '') {
-            $this->filters['weaponType'] = $request->get('weaponType');
-        }
-        if ($request->get('tool') != '') {
-            $this->filters['tool'] = $request->get('tool');
-        }
-        if ($request->get('openMap') != '') {
-            $this->filters['openMap'] = $request->get('openMap');
-        }
     }
 }
