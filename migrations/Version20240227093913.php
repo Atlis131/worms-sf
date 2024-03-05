f714daae255c6bd345c19a92a3e24ec29d52db10
Adam <adam561234@gmail.com>
2024-03-05 14:46:48 +0100

    Swap queries order

--- migrations/Version20240227093913.php
+++ migrations/Version20240227093913.php
@@ -20,8 +20,8 @@ final class Version20240227093913 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE weapon CHANGE min min SMALLINT DEFAULT 1 NOT NULL, CHANGE max max SMALLINT DEFAULT 10 NOT NULL');
         $this->addSql('UPDATE weapon SET min = 1, max = 10 WHERE 1;');
+        $this->addSql('ALTER TABLE weapon CHANGE min min SMALLINT DEFAULT 1 NOT NULL, CHANGE max max SMALLINT DEFAULT 10 NOT NULL');
     }
 
     public function down(Schema $schema): void
